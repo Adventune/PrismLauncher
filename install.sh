27b2e3d85c8e9bdc0e19f5838e230e3954b9f9bc
Adventune <adventunemusics@gmail.com>
2025-07-06 20:08:54 +0300

    feat: :)

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cmake -S . -B build -G Ninja \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="/usr" \ # Use "/usr" when building Linux packages. If building not for package, use "/usr/local"
+-DENABLE_LTO=ON # if you want to enable LTO/IPO
+#  -DLauncher_QT_VERSION_MAJOR="5" # if you want to use Qt 5
+
+cmake --build build
+sudo cmake --install build # Optionally specify DESTDIR for packages (i.e. DESTDIR=${pkgdir} cmake --install ...)
